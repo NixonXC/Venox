3acd4f2e62936edd1dad8626f9d0d1a82d09eb40
NixonXC <invasionyt19@gmail.com>
2022-05-26 18:18:17 +0530

    Add files via upload

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import bridge, commands
 import json
-
+import sys
+import os
 import contextlib
 import io
 
@@ -23,6 +24,21 @@ class admin(commands.Cog):
         except Exception as e:
             return await send(f"```{e.__class__.__name__}: {e}```")
         await send(f'```{str_obj.getvalue()}```')
+
+    @bridge.bridge_command(pass_context=True)
+    @commands.is_owner()
+    async def prefix(self, ctx, *, prefix):
+        try:
+            send = ctx.respond
+        except:
+            send = ctx.reply
+        with open('db/database.json', 'r') as f:
+            config = json.load(f)
+        config['PREFIX'] = prefix
+        with open('db/database.json', 'w') as f:
+            json.dump(config, f, indent=4)
+        unup = config['PREFIX']
+        await send(f"**Prefix changed to `{prefix}` | You will need to restart the bot for it to take effect.**")
 
 def setup(bot):
   bot.add_cog(admin(bot))
